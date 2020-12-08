6fee522e97473304f6bbd3a72427d6885d88573c
Edu531 <dudu53188@gmail.com>
2020-12-07 23:33:59 -0300

    Possible moves of a piece

--- src/borardgame/Piece.java
+++ src/borardgame/Piece.java
@@ -1,6 +1,6 @@
 package borardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -14,9 +14,23 @@ public class Piece {
 		return board;
 	}
 
+	public abstract boolean[][] possiblesMoves();
 	
+	public boolean possibleMove(Position position) {
+		return possiblesMoves()[position.getRow()][position.getColumn()];
+	}
 	
-	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possiblesMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 	
 	
 }
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -14,6 +14,10 @@ public class Rook extends ChessPiece {
 		return "R";
 	}
 	
-	
+	@Override
+	public boolean[][] possiblesMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 	
 }
